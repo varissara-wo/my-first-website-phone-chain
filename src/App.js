d813b699ee61855761d28a1645b4d3aabca78aa0
Varissara <varissara.wo@gmail.com>
2022-09-20 15:09:48 +0700

    addAnswer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  ChakraProvider,
+  Button,
+  Spinner,
+  Stack,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+} from "@chakra-ui/react";
+import { MdBuild } from "react-icons/md";
+import "./App.css";
+import DisplayJohn, { JohnProfile } from "./components/DisplayJohn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ChakraProvider>
+        <Stack direction="row" spacing={4}>
+          <Button colorScheme="blue">Button</Button>
+          <Button colorScheme="cyan">Cyan</Button>
+          <Button leftIcon={<MdBuild />} colorScheme="pink" variant="solid">
+            Settings
+          </Button>
+        </Stack>
+        <Stack direction="row" spacing={4}>
+          <Spinner color="red.500" />
+
+          <Spinner size="xl" />
+
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        </Stack>
+        <Stack direction="row" spacing={4}>
+          <Input placeholder="Basic usage" />
+          <Input variant="filled" placeholder="Filled" />
+          <InputGroup>
+            <InputLeftAddon children="+234" />
+            <Input type="tel" placeholder="phone number" />
+          </InputGroup>
+        </Stack>
+      </ChakraProvider>
+      <DisplayJohn />
+      <JohnProfile />
+    </>
   );
 }
 
